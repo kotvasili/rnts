70edc867ca24d53bdfe193b760e28061426ea106
vasily.solodar <vasili.solodar@effective-soft.com>
2020-10-21 19:39:17 +0300

    icons wip

--- src/navigators/Home.tsx
+++ src/navigators/Home.tsx
@@ -3,11 +3,28 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import FeedScreen from '../screens/Feed';
 import SettingsScreen from '../screens/Settings';
 import {COLORS} from '../styles/colors';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const HomeTabs = createBottomTabNavigator();
 
 const Home: React.FC = () => (
   <HomeTabs.Navigator
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = focused
+            ? 'ios-information-circle'
+            : 'ios-information-circle-outline';
+        } else if (route.name === 'Settings') {
+          iconName = focused ? 'ios-list-box' : 'ios-list';
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+    })}
     tabBarOptions={{
       activeTintColor: COLORS.primary,
       inactiveTintColor: COLORS.primary04,
